be1ce67b08fc0a2cf38d5e57778d1f0a79ef338a
Andrey Rincon <adrincont@unal.edu.co>
2020-04-13 21:22:32 -0500

    cambio 3

--- Analisis descriptivo.py
+++ Analisis descriptivo.py
@@ -6,37 +6,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-### hola andrey, y hola mundo con coronavirus
 # Lectura de los datos --------------------------------------------------------#
 data = pd.read_csv("yelp_photos.csv")
-data.head(2)
-## Numero de reseñas por negocios 
-Num_by_Bss = data[["review_id","business_id"]].groupby("business_id").size()
-pd.crosstab(Num_by_Bss, columns='count').plot.bar()
-plt.xlabel('Numero de opiniones')
-plt.title("Opiniones por negocios")
-plt.legend.remove()
-## Numero de reseñas por fotografia
-Num_by_Photo = data[["review_id","photo_id"]].groupby("photo_id").size()
-## Comportamiento de las estrellas 
-### Comportamiento general de las estrellas
-pd.crosstab(data.stars_review, columns='count').plot.bar()
-plt.legend.remove()
-### Comportamiento de longitud de texto por estrellas
-data_len = data.assign(length=data.text.map(len))
-graph = sns.FacetGrid(data=data_len,col='stars_review')
-graph.map(plt.hist,'length',bins=50,color='blue')
-#------------------------------------------------------------------------------#
-# Parece que, en general, la distribución de la longitud del texto es similar
-# en las cinco clasificaciones. Sin embargo, el número de revisiones de texto
-# parece estar sesgado mucho más hacia las calificaciones de 4 y 5 estrellas. 
-# Esto puede causar algunos problemas más adelante en el proceso
-#------------------------------------------------------------------------------#
-sns.boxplot(x='stars_review', y='length', data=data_len)
 #==============================================================================#
 #=========================== ANALISIS DE PHOTOS ===============================#
 #-Longitud de los comnetarios en la photo--------------------------------------#
-data_photo = df_photos.assign(length=df_photos.caption.map(len))
 data_photo.length[data_photo.length != 0].hist(bins=50)
 #-Numero de photos por business -----------------------------------------------#
 Num_by_Bss = data_photo[["business_id"]].groupby("business_id").size()
