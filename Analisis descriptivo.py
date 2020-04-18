79cda86b25a3c934cc94267d211084ffd69a5e48
Andrey Rincon <adrincont@unal.edu.co>
2020-04-18 17:39:06 -0500

    Update Analisis descriptivo.py

--- Analisis descriptivo.py
+++ Analisis descriptivo.py
@@ -1,56 +1,54 @@
 #=================== Analisis descriptivo de la base ==========================#
 import pandas as pd
-from tqdm import tqdm
-import json
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from textblob import TextBlob
 import re
-from nltk.tokenize import TweetTokenizer
+import nltk
+from nltk import word_tokenize
+from nltk.util import ngrams
+from wordcloud import WordCloud
+from nltk.probability import FreqDist
 tknzr = TweetTokenizer()
 # Lectura de los datos --------------------------------------------------------#
 data = pd.read_csv('datos_finales.csv', engine='python',sep="|")
 #==============================================================================#
-#=========================== ANALISIS DE PHOTOS ===============================#
-#-Longitud de los comnetarios en la photo--------------------------------------#
-data.length.hist(bins=50)
-#-Numero de palabras por comentario--------------------------------------------#
-regex_P= re.compile('[^A-Za-z0-9]')
-def LENW(x):
-    val = regex_P.sub(' ',str(x))
-    long  = tknzr.tokenize(val)
-    return len(long)
-data_len = data.assign(len_W = data.caption.map(LENW))
-data_len[data_len.len_W > 2].len_W.hist(bins=50)
-sns.boxplot(x='stars', y='len_W', data=data_len[data_len.len_W > 2])
-data_len.caption[data_len.len_W == 2]
-#-Numero de photos por business -----------------------------------------------#
-Num_by_Bss = data[["business_id"]].groupby("business_id").size()
-pd.crosstab(Num_by_Bss, columns='count').plot.bar()
-#- Analisis de la longitud de texto por estrellas -----------------------------#
-pd.crosstab(data.stars, columns='count').plot.bar()
-sns.boxplot(x='stars', y='length', data=data)
-data[["stars","length"]].groupby("stars").describe().reset_index()
-#- Calculo de sentimientos y filtro por opiniones objetivas -------------------#
 regex = re.compile('[^A-Za-z0-9 ^. , ^: ^; ^? ^¿ ^¡ ^!]')
-def texCC(x): regex.sub(' ',str(x))
-# Analisis de sentimientos
-def Tf(x):
-    val = TextBlob(str(x)).sentiment.subjectivity
-    return val
-def TfP(x):
-    val = TextBlob(str(x)).sentiment.polarity
-    return val
-data = data.assign(subjectivity =data.caption.map(Tf)) # Tomamos comentarios subjetivos
-data[data.subjectivity != 0].subjectivity.hist(bins=20)
-sns.boxplot(x='stars', y='subjectivity', data=data[data.subjectivity != 0])
-data.caption[data.subjectivity == 0]
+def texCC(x,rg): return rg.sub(' ',str(x))
+# Analisis del corpus ---------------------------------------------------------#
+data_F = data.assign(caption = data.caption.map(lambda p: texCC(p, regex)))
+# Cantidad de palabras por comentario -----------------------------------------#
+def LENW(x):return len(tknzr.tokenize(str(x)))
+data_F = data_F.assign(len_word = data_F.caption.map(LENW))
+pd.crosstab(data_F.len_word, columns='count').plot.bar()
+# Analisis de n-gramas (frecuencias de una palabra) ---------------------------#
+regex2 = re.compile('[^A-Za-z0-9]')
+data_F2 = data.assign(caption = data.caption.map(lambda p: texCC(p, regex2)))
+def NGRAM(x,n):
+    token=nltk.word_tokenize(str(x))
+    bigrams=ngrams(token,n)
+    return list(bigrams)
+def Table_NGRAM(data,n):
+    n_grams = data.caption.map(lambda p: NGRAM(p,1))
+    n_grams = sum(n_grams,[])
+    table_n = pd.DataFrame(FreqDist(n_grams), index =[0]).T
+    table_n.columns = ['Count']
+    table_n = table_n.sort_values('Count', ascending = False)
+    return table_n
 
-data = data.assign(polarity =data.caption.map(TfP)) # Polaridad de las opiniones
-data[data.subjectivity > 0.5].polarity.hist(bins=20)
-sns.boxplot(x='stars', y='polarity', data=data[data.subjectivity > 0.3])
+table_n1 = Table_NGRAM(data,1)
+table_n1[:60].plot.barh()
 
-data.caption[data.polarity < -0.6]
+table_n2 = Table_NGRAM(data,2)
+table_n2[:60].plot.barh()
+
+table_n3 = Table_NGRAM(data,3)
+table_n3[:60].plot.barh()
+
+wordcloud = WordCloud(max_words=250,background_color="white").generate(data_F2.caption.sum())
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
+
 
