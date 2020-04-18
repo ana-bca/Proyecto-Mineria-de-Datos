4b326eb5acb147dec8e6aef8a163341f4d978945
Andrey Rincon <adrincont@unal.edu.co>
2020-04-18 14:02:40 -0500

    Cambios 18

--- Analisis descriptivo.py
+++ Analisis descriptivo.py
@@ -6,12 +6,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from textblob import TextBlob
+import re
+from nltk.tokenize import TweetTokenizer
+tknzr = TweetTokenizer()
 # Lectura de los datos --------------------------------------------------------#
 data = pd.read_csv('datos_finales.csv', engine='python',sep="|")
 #==============================================================================#
 #=========================== ANALISIS DE PHOTOS ===============================#
 #-Longitud de los comnetarios en la photo--------------------------------------#
 data.length.hist(bins=50)
+#-Numero de palabras por comentario--------------------------------------------#
+regex_P= re.compile('[^A-Za-z0-9]')
+def LENW(x):
+    val = regex_P.sub(' ',str(x))
+    long  = tknzr.tokenize(val)
+    return len(long)
+data_len = data.assign(len_W = data.caption.map(LENW))
+data_len[data_len.len_W > 2].len_W.hist(bins=50)
+sns.boxplot(x='stars', y='len_W', data=data_len[data_len.len_W > 2])
+data_len.caption[data_len.len_W == 2]
 #-Numero de photos por business -----------------------------------------------#
 Num_by_Bss = data[["business_id"]].groupby("business_id").size()
 pd.crosstab(Num_by_Bss, columns='count').plot.bar()
@@ -20,11 +34,8 @@ pd.crosstab(data.stars, columns='count').plot.bar()
 sns.boxplot(x='stars', y='length', data=data)
 data[["stars","length"]].groupby("stars").describe().reset_index()
 #- Calculo de sentimientos y filtro por opiniones objetivas -------------------#
-from textblob import TextBlob
-import re
 regex = re.compile('[^A-Za-z0-9 ^. , ^: ^; ^? ^¿ ^¡ ^!]')
 def texCC(x): regex.sub(' ',str(x))
-data_photo.caption = data.caption.map(texCC)
 # Analisis de sentimientos
 def Tf(x):
     val = TextBlob(str(x)).sentiment.subjectivity
@@ -35,7 +46,11 @@ def TfP(x):
 data = data.assign(subjectivity =data.caption.map(Tf)) # Tomamos comentarios subjetivos
 data[data.subjectivity != 0].subjectivity.hist(bins=20)
 sns.boxplot(x='stars', y='subjectivity', data=data[data.subjectivity != 0])
+data.caption[data.subjectivity == 0]
 
 data = data.assign(polarity =data.caption.map(TfP)) # Polaridad de las opiniones
-data[data.subjectivity > 0.3].polarity.hist(bins=20)
+data[data.subjectivity > 0.5].polarity.hist(bins=20)
 sns.boxplot(x='stars', y='polarity', data=data[data.subjectivity > 0.3])
+
+data.caption[data.polarity < -0.6]
+
