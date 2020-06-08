9dfb82ed5211a8ca19e20bc6a9265f66323fab9a
adrincont <adrincont@unal.edu.co>
2020-06-08 13:17:38 -0500

    modelo

--- Clasificacion de Palabras compuestas.py
+++ Clasificacion de Palabras compuestas.py
@@ -9,15 +9,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 #======================== Clasificador de bigramas ===========================#
 vocabul = pd.read_csv('palabras_finales.csv', engine='python',sep="|")
-# Creamos el diccionario de palabras con respectivo codigo
-vocabul = np.array(vocabul.word)
-vectorizer = CountVectorizer()
-vectorizer.fit(vocabul)
-vectorizer.vocabulary_
 # Vectores en los bigramas
-data = pd.read_csv('palabrascompu.csv',sep=",")
+data = pd.read_csv('palabrascompu2.csv',sep=",")
 
-sentences = data['palabracom'].values
+sentences = data['palabracompu'].values
 sentences
 y = data['Clase'].values
 sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25, random_state=1000)
@@ -27,12 +22,17 @@ vectorizer.fit(sentences_train)
 
 X_train = vectorizer.transform(sentences_train)
 X_test  = vectorizer.transform(sentences_test)
-X_train
 
-classifier = LogisticRegression()
-classifier.fit(X_train, y_train)
-score = classifier.score(X_test, y_test)
-print("Accuracy:", score)
+from keras.preprocessing.text import Tokenizer
+tokenizer = Tokenizer(num_words= 991)
+tokenizer.fit_on_texts(sentences_train)
+
+X_train = tokenizer.texts_to_sequences(sentences_train)
+X_test = tokenizer.texts_to_sequences(sentences_test)
+from keras.preprocessing.sequence import pad_sequences
+maxlen = 2
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
 
 from keras.models import Sequential
 from keras import layers
@@ -40,46 +40,28 @@ from keras import layers
 input_dim = X_train.shape[1]  # Number of features
 
 model = Sequential()
-model.add(layers.Dense(20, input_dim=input_dim, activation='relu'))
+model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
 model.summary()
 
 history = model.fit(X_train, y_train,
-                    epochs=500,
-                    verbose=False,
+                    epochs=50,
+                    verbose=1,
+                    validation_split=0.1,
+                    shuffle=False,
                     validation_data=(X_test, y_test),
-                    batch_size=100)
+                    batch_size=15)
 
 loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
 print("Training Accuracy: {:.4f}".format(accuracy))
 loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
 print("Testing Accuracy:  {:.4f}".format(accuracy))
 
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-
-def plot_history(history):
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    x = range(1, len(acc) + 1)
-    plt.figure(figsize=(12, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(x, acc, 'b', label='Training acc')
-    plt.plot(x, val_acc, 'r', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    plt.subplot(1, 2, 2)
-    plt.plot(x, loss, 'b', label='Training loss')
-    plt.plot(x, val_loss, 'r', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
-    
-plot_history(history)
-
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
 # Predicciones
 y_pred = model.predict(X_test)
 pd.DataFrame(data = {'pred':y_pred.reshape(100,),'test':y_test})
