92ae6e39634e2440f3ad6ac16cdd0727046f963a
Ana María Barbosa Cárdenas <62305582+ana-bca@users.noreply.github.com>
2020-06-22 11:48:17 -0500

    por frecuencias

--- recorte_datos.py
+++ recorte_datos.py
@@ -16,13 +16,13 @@ nlp = English()
 
 
 #### lectura de datos
-data = pd.read_csv('data_F.csv', engine='python',sep=",") ## Lectura de los datos
-palabras = pd.read_csv('palabras_finales.csv', engine='python',sep="|") ## Lectura de los datos
+data = pd.read_csv('data_photo_F.csv', engine='python',sep="\t") ## Lectura de los datos
+palabras = pd.read_csv('palabras_finales.csv', engine='python',sep="\t") ## Lectura de los datos
 netique=40
 etiquetas=palabras[:netique]
 data.shape[0]
 
-data_final=pd.DataFrame(columns=('photo_id', 'business_id', 'caption', 'label', 'length', 'stars','len_word'))
+data_final=pd.DataFrame(columns=('photo_id', 'caption', 'label', 'length', 'stars','len_word'))
 for i in range(data.shape[0]):
     cap= nltk.word_tokenize(data.caption[i]) 
     for j in range(len(cap)):
@@ -38,3 +38,17 @@ from data_final
 group by photo_id ''')
 csv_datos_recortados = "data_final.csv"
 data_final.to_csv(csv_datos_recortados, index=False,sep=",")
+
+etiquetas.loc['cuenta']=0
+data_final=pd.DataFrame(columns=('photo_id', 'caption', 'label', 'length', 'stars','len_word'))
+for i in range(data.shape[0]):
+    cap= nltk.word_tokenize(data.caption[i]) 
+    for j in range(len(cap)):
+       for k in range(netique):
+           if cap[j]==etiquetas.word[k]:
+               etiquetas.cuenta[k]=etiquetas.cuenta[k]+1
+               if etiquetas.cuenta[k]<700:
+                    data_final.loc[len(data_final)]=data.loc[i]
+
+data_final.caption
+etiquetas
